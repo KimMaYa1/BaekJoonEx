12c2e6017bf0c6a13e852731b0a7feca6d858d20
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-24 19:18:04 +0900

    [Bronze V] Title: 사칙연산, Time: 72 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,7 @@
+string[] str = Console.ReadLine().Split();
+int a = int.Parse(str[0]), b = int.Parse(str[1]);
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
