05bf08f67e5b0bb595ebf1cb58a9f0ac84f384eb
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-24 10:24:28 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n != 0)
+        while(n > 0)
         {
             answer += n % 10;
             n = n / 10;
