3f9e6d955ca2c9482bb1f6a356b3ef697eb35b94
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-24 20:23:54 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,11 @@
+int a = int.Parse(Console.ReadLine());
+if(a <= 100 && a > 89)
+    Console.WriteLine("A");
+else if(a > 79)
+    Console.WriteLine("B");
+else if(a > 69)
+    Console.WriteLine("C");
+else if(a > 59)
+    Console.WriteLine("D");
+else
+    Console.WriteLine("F");
