b48107402dda94b3650b93a683f82fd40c77e944
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-24 20:39:55 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.83 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cs
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cs
@@ -1,6 +1,23 @@
 public class Solution {
     public int solution(string s) {
-        int answer = int.Parse(s);
+        int answer = 0;
+        string str = s.Substring(0,1);
+        
+        
+        if(int.TryParse(str, out int x))
+        {
+            str = s.Substring(0, s.Length);
+            answer = int.Parse(str);
+        }
+        else
+        {
+            str = s.Substring(1, s.Length-1);
+            answer = int.Parse(str);
+            if(s[0] == '-')
+            {
+                answer = answer * -1;
+            }
+        }
         
         return answer;
     }
