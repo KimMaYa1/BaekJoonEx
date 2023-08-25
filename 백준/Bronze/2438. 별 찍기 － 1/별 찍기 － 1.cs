ba3bb0f63ac4600f35c98f3adf8514debdc80e33
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-25 18:26:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -1,9 +1,16 @@
+Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+
 int num = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i <= num; i++)
+for(int i=0;i<num;i++)
 {
-    for(int j = 0; j < i; j++){
+    Console.Write("*");
+    for(int j=0;j<i;j++)
+    {    
         Console.Write("*");
     }
     Console.WriteLine();
 }
+
+
+Console.Out.Flush();
