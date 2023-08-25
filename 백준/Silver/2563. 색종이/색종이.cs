3c4eb577342e5f64856b9284edb098d13b93d528
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-25 15:40:38 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 5860 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.cs
@@ -0,0 +1,32 @@
+int arr = int.Parse(Console.ReadLine());
+int area = 0;
+int[][] pan = new int[100][];
+int x, y;
+for(int i = 0; i < pan.Length; i++)
+{
+    pan[i] = new int[100];
+}
+for(int i = 0; i < arr; i++)
+{
+    string[] str = Console.ReadLine().Split();
+    x = int.Parse(str[0]);
+    y = int.Parse(str[1]);
+    for(int j = 0; j < 10; j++)
+    {
+        for(int z = 0; z < 10; z++)
+        {
+            pan[x + j][y + z] = 1;
+        }
+    }
+}
+for(int i = 0; i < pan.Length; i++)
+{
+    for(int j = 0; j< pan[i].Length; j++)
+    {
+        if(pan[i][j] == 1)
+        {
+            area++;
+        }
+    }
+}
+Console.WriteLine(area);
