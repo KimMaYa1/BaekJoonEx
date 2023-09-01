3bdb83026de0bdf8fbac915a5b72e350d45ca6ec
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-09-01 10:02:19 +0900

    [level 1] Title: 시저 암호, Time: 40.82 ms, Memory: 47.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Solution {
+    public string solution(string s, int n) {
+        string answer = "";
+        foreach(char c in s){
+            if(c != ' ')
+            {
+                int tmp;
+                if((int)c<91)
+                {
+                    tmp = (int)c + n;
+                    if(tmp>90)
+                        tmp = 64 + (tmp-90);
+                }
+                else
+                {
+                    tmp = (int)c + n;
+                    if(tmp>122)
+                        tmp = 96 + (tmp-122);
+                }
+                answer += Convert.ToChar(tmp);
+            }else{
+                answer += c;
+            }
+        }
+        return answer;
+    }
+}
