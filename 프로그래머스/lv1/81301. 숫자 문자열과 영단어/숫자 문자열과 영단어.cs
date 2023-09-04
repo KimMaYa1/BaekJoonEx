8bda9ba4671adbb568793fe9ab8c9a9bb05495ba
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-09-04 20:36:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.75 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(string s) {
+        int answer = 0;
+        s = s.Replace("zero", "0").Replace("one", "1").Replace("two", "2").Replace("three", "3").Replace("four", "4").Replace("five", "5").Replace("six", "6").Replace("seven", "7").Replace("eight", "8").Replace("nine", "9");
+        answer = int.Parse(s);
+        return answer;
+    }
+}
