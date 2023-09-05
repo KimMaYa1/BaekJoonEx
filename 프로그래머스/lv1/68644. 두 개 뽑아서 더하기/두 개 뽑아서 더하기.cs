fd6410ca785a64e9f176c125877a757817f62552
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-09-05 09:13:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.11 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        List<int> answer = new List<int>();
+        for(int i = 0; i < numbers.Length; i++){
+            for(int j = i+1; j < numbers.Length; j++){
+                answer.Add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        int[] temp = answer.Distinct().ToArray();
+        Array.Sort(temp);
+        return temp;
+    }
+}
