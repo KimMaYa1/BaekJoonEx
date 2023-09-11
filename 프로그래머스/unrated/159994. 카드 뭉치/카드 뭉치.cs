ba8a5a912fedd5cf5c103578011aea422e90dfdb
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-09-11 09:26:03 +0900

    [unrated] Title: 카드 뭉치, Time: 0.20 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cs
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cs
@@ -2,34 +2,20 @@ using System;
 
 public class Solution {
     public string solution(string[] cards1, string[] cards2, string[] goal) {
-        string answer = "";
-        string go = "";
+        string answer = "Yes";
         int count1 = 0;
         int count2 = 0;
         foreach(string str in goal){
-            go += str;
-            if(count1 >= cards1.Length){
-                count1 = cards1.Length-1;
-            }else if(count2 >= cards2.Length){
-                count2 = cards2.Length-1;
-            }
-            
-            if(str.Equals(cards1[count1])){
-                answer +=cards1[count1];
+            if(count1 < cards1.Length&& str.Equals(cards1[count1])){
                 count1++;
             }
-            else if(str.Equals(cards2[count2])){
-                answer +=cards2[count2];
+            else if(count2 < cards2.Length && str.Equals(cards2[count2])){
                 count2++;
             }
             
             else{
                 answer = "No";
-                break;
             }
-        }
-        if(answer.Equals(go)){
-                answer = "Yes";
         }
         return answer;
     }
